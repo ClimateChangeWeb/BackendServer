056e95d1c4fafe904b0c9749653924a33831c38d
Ray Guo <ma2808203259@hotmail.com>
2021-08-22 01:23:03 +0800

    rss sub finished

---
+++ models/australiaWarningRss.js
@@ -0,0 +1,11 @@
+const australiaRss = [
+  'http://www.bom.gov.au/fwo/IDZ00054.warnings_nsw.xml',
+  'http://www.bom.gov.au/fwo/IDZ00055.warnings_nt.xml',
+  'http://www.bom.gov.au/fwo/IDZ00059.warnings_vic.xml',
+  'http://www.bom.gov.au/fwo/IDZ00056.warnings_qld.xml',
+  'http://www.bom.gov.au/fwo/IDZ00060.warnings_wa.xml',
+  'http://www.bom.gov.au/fwo/IDZ00057.warnings_sa.xml',
+  'http://www.bom.gov.au/fwo/IDZ00058.warnings_tas.xml',
+];
+
+module.exports = australiaRss;
--- routers.js
+++ routers.js
@@ -5,6 +5,9 @@ const axios = require('axios');
 const DiscoverModel = require('./models/discover');
 var fs = require('fs');
 var path = require('path');
+const rssUrls = require('./models/australiaWarningRss');
+const { parse } = require('rss-to-json');
+const { url } = require('inspector');
 
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {
   flags: 'a',
@@ -61,5 +64,59 @@ router.get('/discover', (req, res) => {
       console.log(error);
     });
 });
+
+router.get('/warning', async (req, res) => {
+  let results = []; // array to store the result
+  let count = 0; // count the number of states
+  let validURL; // store the valid url for insert into results
+  let add = false;
+
+  for await (const url of rssUrls) {
+    await axios
+      .get(url)
+      .then(async function (response) {
+        // The rss to json package cannot handling for empty xml
+        // so I have to do it by myself
+        count++;
+        if (findWord('<item>', response.data)) {
+          validURL = url;
+          console.log('true');
+          add = true;
+        } else {
+          console.log(url);
+          console.log('false');
+          add = false;
+        }
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      });
+
+    // if add is true then add the rss data to list
+    if (add) {
+      var rss = await parse(validURL);
+      await results.push(rss);
+      console.log(results[0].items[0]);
+    }
+
+    // if every get every data send to the client
+    if (count === 7) {
+      res.send(results);
+      console.log(results);
+    }
+  }
+
+  //await res.send(results);
+});
+
+//check if the rss sub
+function findWord(word, str) {
+  if (str.search(word) === -1) {
+    return false;
+  } else {
+    return true;
+  }
+}
 
 module.exports = router;
